b1aabec388b1c19e85cb3a163849eec25c811555
uliana <uliankadubrovska@gmail.com>
2024-02-01 22:29:50 +0200

    improver

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 AOS.init();
+jQuery(document).ready(function(){
+    jQuery(window).scroll(function () {
+        if (jQuery(this).scrollTop() > 600) {
+            jQuery('#myBtn').css('opacity', '1');
+        } else {
+            jQuery('#myBtn').css('opacity', '0');
+        }
+    });
+
+    jQuery('#myBtn').click(function () {
+        jQuery('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+});
